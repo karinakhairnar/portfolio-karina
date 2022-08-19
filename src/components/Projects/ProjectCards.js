7ed3d3e305d9e8e98716dafef4b4b31aa82bf5f2
karinakhairnar <karinakhairnar@gmail.com>
2022-08-20 00:51:15 +0530

    working

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,21 +1,34 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { BiLinkExternal } from "react-icons/bi";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { BiLinkExternal } from 'react-icons/bi';
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+    <Card className='project-card-view' style={{padding:'1em'}}>
+      <Card.Img variant='top' src={props.imgPath} alt='card-img' />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+        <Card.Text style={{ textAlign: 'justify' }}>{props.info}</Card.Text>
+        <Card.Text style={{ textAlign: 'justify' }}>
+          <h5 style={{ color: 'purple' }}>Features :-</h5> {props.feature}
         </Card.Text>
-        <Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> &nbsp;
-          {props.isBlog ? "View Blog" : "View Project"}
-        </Button>
+        <Card.Text style={{ textAlign: 'justify' }}>
+          <h5 style={{ color: 'purple' }}>Areas of responsibility :-</h5>{' '}
+          {props.res}
+        </Card.Text>
+        <div style={{ textAlign: 'center', display: 'flex',justifyContent: 'space-between',padding:'1em'}}>
+          <div>
+            <Button variant='primary' href={props.link} target='_blank'>
+            Github <BiLinkExternal /> &nbsp;
+            </Button>
+          </div>
+          <div>
+            <Button variant='primary' href={props.isBlog} target='_blank'>
+              Blogs<BiLinkExternal /> &nbsp;
+            </Button>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,63 +20,84 @@ function Projects() {
           <Col md={4} className='project-card'>
             <ProjectCard
               imgPath={credo}
-              isBlog={false}
+              isBlog='https://medium.com/@karinakhairnar/clean-beauty-website-969ed17085d5'
               title='Clean Beauty Website'
-              description='We live for discovering the best, cleanest indie and luxury brands around, and carry the largest collection of safe, non-toxic beauty products. A collaborative project built by a team of 5 developers executed in 7 days.'
+              info='We live for discovering the best, cleanest indie and luxury brands around, and carry the largest collection of safe, non-toxic beauty products. So you can play, get inspired and find that you love clean beauty.A collaborative project built by a team of 5 members executed in 7 days.'
+              feature='
+              Functionality of Filtering 
+              | Functionality of Hovering
+              | Media Query
+              | Sign-in/Sign-up Authentication
+              | Add-To-Card 
+              | Payment '
+              res='
+              Build Checkout Page - In Checkout page related to payment and shipping/billing details , it basically collects customers shipping details. 
+              | Build Payment page - In payment page customers are allowed to purchase items easily and secretly.
+              | Change the CSS of every page for getting same look as original site
+              Tech Stack- HTML | CSS | Javascript '
               link='https://github.com/Thannzz/credo-beauty-clone'
             />
           </Col>
-
           <Col md={4} className='project-card'>
             <ProjectCard
               imgPath={myntra}
-              isBlog={false}
+              isBlog='https://medium.com/@karinakhairnar/myntra-online-fashion-store-dfc32d20fa8b'
               title='Myntra online Fashion Store'
-              description="This is a Clone website of myntra.com built by 4 developers of the team including me in 6 days. It's an online fashion store where users can come and buy products related to beauty care, fashion, Home living, etc."
+              info='This is an Clone website of myntra.com which are built by 4 members of team including me in 6 days ,Its an online fashion store where user can come and buy products related to beauty care , fashion , Home living, etc.'
+              feature='Signin and Signup Authentication.
+              | Navigation bar for exploring.
+              | User can add products to cart.
+              | Sorting the products by price and brands.
+              | User can place orders.'
+              res='Signin and Signup Authentication.
+              | Add to cart - user can add product in card/bag.
+              | WishList Page - if user links products that can be store in this page.
+              | Change CSS in some of the pages.
+              Tech Stack- HTML | CSS | Javascript '
               link='https://github.com/yashtomar15/myntra-clone'
             />
           </Col>
-
           <Col md={4} className='project-card'>
             <ProjectCard
               imgPath={uboric}
-              isBlog={false}
+              isBlog='https://medium.com/@karinakhairnar/uboric-com-clone-4543c72bbfe9'
               title='Uboric.com- Clone'
-              description='Here at ‘Uboric’.com, we realize that style and prosperity begin with the correct shoes. We likewise understand that effectively finding the size and style to meet your requirements is vital to your web based shopping knowledge. Since beginning our web based business website , we’ve been endeavoring to present to you that perfect shopping experience.'
+              info='On Uboric you can Shop Online for Men & Women Clothing, Shoes, fashion, Home Decor & Kitchen Appliances & Much More. A collaborative project built by a team of 6 members executed in 7 days.'
+              feature='Sign-in/Sign-up Authentication | Functionality of Filtering | Functionality of Hovering | Add-To-Card | Payment'
+              res=' Add to cart - user can add product in card/bag. | Build Checkout Page - In Checkout page related to payment and shipping/billing details , it basically collects customers shipping details | Build Payment page - In payment page customers are allowed to purchase items easily and secretly | Change CSS in some of the pages | Team Leading'
               link='https://github.com/Saraswati121/Uboric'
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              imgPath={uboric}
+              isBlog='https://medium.com/@karinakhairnar/karina-khairnar-4f7349a02b9b'
+              title='
+              Clone of Cronometer'
+              info='This was a collaborative construct week project at Masai School. In this project, we have tried to create a full stack clone of the cronometerwebsite using ReactJs for the front-end and NodeJs, ExpressJs, and MongoDB for the backend. This project was built by a team of 4 developers in 5 Days.'
+              feature='Sign-in/Sign-up Authentication | Functionality of Hovering | Add-To-Card | Payment'
+              res='Building Sign-up and Login Page with all functionality.
+              | Storing all data in MongoDB server also in Localstorage.
+              | Providing Authentication to both pages by using the backend.
+              Tech Stack: React | Redux | Styled Component | Chakra UI | Node.JS | Express.JS | Mongoose | MongoDB'
+              link='https://github.com/Mohd-Haroon/gusty-adjustment-6018'
             />
           </Col>
-
-          <Col md={4} className="project-card">
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              imgPath={uboric}
+              isBlog='https://medium.com/@callmeabhi1997/cloning-kimaye-website-ff45bb15c8e'
+              title='Clone OF Kimaye'
+              info='This was a collaborative construct week project at Masai School. In this project, we have tried to create a full stack clone of the Kimaye website using ReactJs for the front-end and NodeJs, ExpressJs, and MongoDB for the backend. This project was built by a team of 5 developers in 5 Days.'
+              feature='Sign-in/Sign-up Authentication | Functionality of Hovering | Add-To-Card | Payment'
+              res='Building a Cart Page with all functionality.
+              | Building Checkout and Shipping Page with all functionality.
+              | Building Learn and Grow Pages with functionality.
+              | Providing CSS on the Same pages.
+              Tech Stack: React | Redux | Styled Component | Chakra UI | Node.JS | Express.JS | Mongoose | MongoDB'
+              link='https://github.com/Mohd-Haroon/gusty-adjustment-6018'
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
